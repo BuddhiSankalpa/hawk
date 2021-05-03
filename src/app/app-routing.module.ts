804811a35cff0dac565092a4f5456d4db0df45ec
shehan <asrajakaruna@gmail.com>
2021-05-04 00:23:16 +0530

    register,login,about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'about', component: RegisterComponent },
+  { path: 'about', component: AboutComponent },
   { path: '', component: HomeComponent }
 ];
 
