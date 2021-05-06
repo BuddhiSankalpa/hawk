f8369ba16d04413634e1602d472cafad87dd07d5
shehan <asrajakaruna@gmail.com>
2021-05-06 22:34:06 +0530

    js solved

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  // constructor() { }
 
-  ngOnInit(): void {
-  }
+  // ngOnInit(): void {
+  // }
+
+  ngOnInit() { 
+    this.loadJsFile("../../assets/js/jquery.meanmenu.js") 
+    this.loadJsFile("../../assets/js/plugins.js") 
+    this.loadJsFile("../../assets/js/magnific.min.js") 
+    this.loadJsFile("../../assets/js/bootstrap.min.js") 
+    this.loadJsFile("../../assets/js/jquery.steller.min.js") 
+    this.loadJsFile("../../assets/js/mapcode.js") 
+    this.loadJsFile("../../assets/js/owl.carousel.min.js") 
+    this.loadJsFile("../../assets/js/wow.min.js") 
+    this.loadJsFile("../../assets/js/main.js");  
+  }  
+  public loadJsFile(url) {  
+    let node = document.createElement('script');  
+    node.src = url;  
+    node.type = 'text/javascript';  
+    document.getElementsByTagName('head')[0].appendChild(node);  
+  }  
 
 }
