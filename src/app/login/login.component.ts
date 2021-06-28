cdd456bafc878fb0be7da528edec2adeffdac592
ashfaqk <karthaj@2212>
2021-06-28 21:49:17 +0530

    new ui changes, toggle menu fix

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -23,19 +23,19 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  public setTab2( ){
+  public setTab1( ){
   document.getElementById('registration').style.display = "block";   
     document.getElementById('login').style.display = "none";   
-     document.getElementById('tab-btn-1').classList.remove('selected')  ;
-     document.getElementById('tab-btn-2').classList.add('selected')  ;
+     document.getElementById('tab-btn-1').classList.add('selected')  ;
+     document.getElementById('tab-btn-2').classList.remove('selected')  ;
   
   }
 
-  public setTab1( ){
+  public setTab2( ){
   document.getElementById('registration').style.display = "none";    
     document.getElementById('login').style.display = "block";   
-    document.getElementById('tab-btn-2').classList.remove('selected')  ;
-    document.getElementById('tab-btn-1').classList.add('selected')  ;
+    document.getElementById('tab-btn-2').classList.add('selected')  ;
+    document.getElementById('tab-btn-1').classList.remove('selected')  ;
   }
 
 }
