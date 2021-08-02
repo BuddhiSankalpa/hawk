092fc75737ff7055f18eb14cf2104f585688954b
shehan <asrajakaruna@gmail.com>
2021-08-02 23:35:13 +0530

    education

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -12,4 +12,23 @@ export class EducationComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  public setTab1() {
+    document.getElementById('beginner').style.display = 'flex';
+    document.getElementById('advance').style.display = 'none';
+
+    document.getElementById('tab-btn-1').classList.add('selected');
+    document.getElementById('tab-btn-2').classList.remove('selected');
+  }
+
+  public setTab2() {
+    document.getElementById('beginner').style.display = 'none';
+    document.getElementById('advance').style.display = 'flex';
+
+
+    document.getElementById('tab-btn-1').classList.remove('selected');
+    document.getElementById('tab-btn-2').classList.add('selected');
+  }
+
+
 }
