60074d60bd79afb231c18a47f4c236831b97b9da
Tharindu Rasanga <trasanga95@gmail.com>
2021-09-05 21:34:33 +0530

    api changes

--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -11,18 +11,18 @@ export class ApiService{
   constructor(private http: HttpClient) { }
 
   register(regObj: any):Observable<any>{
-    return this.http.post(`${baseUrl}/register`, regObj);
+    return this.http.post(`${baseUrl}/auth/register`, regObj);
   }
 
   login(email: any, password: any):Observable<any>{
-    return this.http.post(`${baseUrl}/auth?email=${email}&password=${password}`,{});
+    return this.http.post(`${baseUrl}/auth/login?email=${email}&password=${password}`,{});
   }
 
   verify(email: any, verificationCode: any):Observable<any>{
-    return this.http.post(`${baseUrl}/verify?email=${email}&verificationCode=${verificationCode}`,{});
+    return this.http.post(`${baseUrl}/auth/verify?email=${email}&verificationCode=${verificationCode}`,{});
   }
 
   getNewsLine() : Observable<any>{
-    return this.http.get(`${baseUrl}/news/getAll`);
+    return this.http.get(`${baseUrl}/announcements/news/getAll`);
   }
 }
