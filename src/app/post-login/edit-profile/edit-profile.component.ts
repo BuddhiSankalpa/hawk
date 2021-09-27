6bc01ba88977bc5e456317a71ae0e4c0d5d34849
shehan <asrajakaruna@gmail.com>
2021-09-27 21:28:05 +0530

    edit prof

--- src/app/post-login/edit-profile/edit-profile.component.ts
+++ src/app/post-login/edit-profile/edit-profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { Subject } from 'rxjs';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-edit-profile',
@@ -9,11 +11,23 @@ import { Subject } from 'rxjs';
 })
 export class EditProfileComponent implements OnInit {
 
+  registerForm: FormGroup;
+
   onClose: Subject<boolean>;
   constructor(private modalRef: BsModalRef) { }
 
   ngOnInit() {
     this.onClose = new Subject();
+    this.registerForm = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      phone: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+      repassword: new FormControl('', Validators.required),
+      //userType: new FormControl('', Validators.required),
+      terms: new FormControl('', Validators.requiredTrue),
+    });
   }
 
   closeModal(value: boolean) {
@@ -21,4 +35,7 @@ export class EditProfileComponent implements OnInit {
     this.modalRef.hide();
   }
 
+  updateProfile =()=>{
+
+  }
 }
